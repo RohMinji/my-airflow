9c1d802160bc5d1974dfa9241f9d476e0f619e2c
rohminji <57671092+RohMinji@users.noreply.github.com>
2022-12-20 09:40:00 +0900

    Update and rename helloworld.py to hello_world.py

---
+++ dags/hello_world.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello_world():
+    print('Hello World')
+    
+with DAG(dag_id="hello_world",
+         start_date=datetime(2022,12,20),
+         schedule_interval="@hourly",
+         catchup=False) as dag:
+        task1=PythonOperator(
+            task_id="hello_world",
+            python_callable=helloWorld)
+        
+task1
