7f73e396cc4e8d28e148d7fed1db17dc92479036
rohminji <57671092+RohMinji@users.noreply.github.com>
2023-01-09 17:10:03 +0900

    Update pod_test4.py

--- dags/pod_test4.py
+++ dags/pod_test4.py
@@ -13,24 +13,25 @@ default_args = {
   'retry_delay'           : timedelta(hours=1)
 }
 with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *", catchup=False) as dag:
-t1 = BashOperator(
-  namespace='mj-proj',
-  task_id='print_current_date',
-  bash_command='date'
-)
-t2 = DockerOperator(
-  namespace='mj-proj',
-  task_id='docker_command',
-  image='ghcr.io/rohminji/icis_samp:master',
-  api_version='auto',
-  auto_remove=True,
-  command="/bin/sleep 30",
-  docker_url="unix://var/run/docker.sock",
-  network_mode="bridge"
-)
-t3 = BashOperator(
-  namespace='mj-proj',
-  task_id='print_hello',
-  bash_command='echo "hello world"'
-)
-t1 >> t2 >> t3
+  t1 = BashOperator(
+    namespace='mj-proj',
+    task_id='print_current_date',
+    bash_command='date'
+  )
+
+  t2 = DockerOperator(
+    namespace='mj-proj',
+    task_id='docker_command',
+    image='ghcr.io/rohminji/icis_samp:master',
+    api_version='auto',
+    auto_remove=True,
+    command="/bin/sleep 30",
+    docker_url="unix://var/run/docker.sock",
+    network_mode="bridge"
+  )
+  t3 = BashOperator(
+    namespace='mj-proj',
+    task_id='print_hello',
+    bash_command='echo "hello world"'
+  )
+  t1 >> t2 >> t3
