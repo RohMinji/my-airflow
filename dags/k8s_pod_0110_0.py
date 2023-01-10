e80d66c8239e90b02dc48ff403ed4019acb67639
rohminji <57671092+RohMinji@users.noreply.github.com>
2023-01-10 14:26:01 +0900

    Update k8s_pod_0110_0.py

--- dags/k8s_pod_0110_0.py
+++ dags/k8s_pod_0110_0.py
@@ -2,13 +2,14 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 
 k = KubernetesPodOperator(
     name="hello-dry-run",
-    image="debian",
+    image="ghcr.io/rohminji/nginx:master",
     cmds=["bash", "-cx"],
     arguments=["echo", "10"],
     labels={"foo": "bar"},
     task_id="dry_run_demo",
     do_xcom_push=True,
     namespace="mj-proj",
+    get_logs=True,
 )
 
 k.dry_run()
