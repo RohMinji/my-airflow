00cb461480f7d050c9b43acec106ee7c2c26d099
노민지 <rohminji@rohminjiui-MacBookPro.local>
2023-02-16 17:35:28 +0900

    edit rocket.py

--- dags/rocket.py
+++ dags/rocket.py
@@ -19,8 +19,7 @@ dag = DAG(
 
 download_launches = BashOperator(
     task_id='download_launches',
-    bash_command="curl -o /tmp/launches.json -L \
-        'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", dag=dag,
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", dag=dag,
 )
 
 #json 결괏값을 파싱하고 모든 로켓 사진을 다운로드하는 함수
